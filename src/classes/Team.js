1064d78d008474f25fd95498613406fbbc2e54d8
DaveyD24 <69884791+DaveyD24@users.noreply.github.com>
2025-06-15 19:38:18 +1000

    Removed Irrelevant Files

--- src/classes/Team.js
+++
@@ -1,7 +0,0 @@
-class Team {
-    constructor(localName, teamName) {
-        this.localName = localName;
-        this.teamName = teamName === "Wests Tigers" ? "Tigers" : teamName;
-    }
-}
-module.exports = {Team};
