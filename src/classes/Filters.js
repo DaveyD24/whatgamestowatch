8ebe41d23209fe276b575263723b35ecd047c628
Davey <dtdyer123@gmail.com>
2025-11-22 13:09:30 +0700

    2026 MVP

---
+++ src/classes/Filters.js
@@ -0,0 +1,288 @@
+import { DocumentElement } from "./Elements.js";
+import { allTeams } from "./Data.js";
+import { globalData } from "./2026data.js";
+export class Filters {
+    constructor() {
+        this.state = "NSW";
+        this.teams = [];
+        this.stadiums = [];
+        this.upcoming = true;
+        this.toggle = "round";
+    }
+    ChangeState(state) {
+        this.state = state;
+        this.stadiums = [];
+        this.OnChange();
+    }
+    AddTeams(teams) {
+        for (const team of teams) {
+            this.teams.push(team);
+        }
+        this.OnChange();
+    }
+    RemoveTeams(teams) {
+        for (const team of teams) {
+            this.teams.splice(this.teams.indexOf(team), 1);
+        }
+        this.OnChange();
+    }
+    AddStadiums(stadiums) {
+        for (const stadium of stadiums) {
+            this.stadiums.push(stadium);
+        }
+        this.OnChange();
+    }
+    RemoveStadiums(stadiums) {
+        for (const stadium of stadiums) {
+            this.stadiums.splice(this.stadiums.indexOf(stadium), 1);
+        }
+        this.OnChange();
+    }
+    ToString() {
+        return `${this.state}\n${this.teams}\n${this.stadiums}`;
+    }
+    OnChange() {
+        console.log(this.ToString());
+        const teamContainer = document.querySelector(".team-container");
+        if (teamContainer === null) {
+            return;
+        }
+        teamContainer.innerHTML = "";
+        for (const team of allTeams) {
+            const element = DocumentElement({
+                elementType: "div",
+                classList: ["team-option"],
+                id: team.teamName,
+                children: [
+                    DocumentElement({ elementType: "div", className: "logo", children: [
+                            DocumentElement({ elementType: "img", attribute: { name: "src", value: `image/${team.teamName}.png` }, })
+                        ] })
+                ],
+                listener: { event: "click", callBack: () => {
+                        if (this.teams.includes(team.teamName)) {
+                            this.RemoveTeams([team.teamName]);
+                        }
+                        else {
+                            this.AddTeams([team.teamName]);
+                        }
+                    } }
+            });
+            if (this.teams.includes(team.teamName)) {
+                element.classList.add("team-selected");
+            }
+            else {
+                element.classList.remove("team-selected");
+            }
+            teamContainer === null || teamContainer === void 0 ? void 0 : teamContainer.appendChild(element);
+        }
+        const stadiumContainer = document.querySelector(".stadium-options-container");
+        if (stadiumContainer === null) {
+            return;
+        }
+        stadiumContainer.innerHTML = "";
+        for (const stadium of GetStadiumsByTeamsAndState(this.state, this.teams)) {
+            const element = DocumentElement({
+                elementType: "div",
+                className: "stadium-option",
+                children: [
+                    DocumentElement({
+                        elementType: "div",
+                        classList: ["bar-overlay", "stadium-unselected"],
+                        children: [
+                            DocumentElement({ elementType: "div", className: "circle-large", children: [
+                                    DocumentElement({ elementType: "div", className: "circle-small" })
+                                ] })
+                        ]
+                    }),
+                    DocumentElement({
+                        elementType: "div",
+                        className: "stadium-details",
+                        children: [
+                            DocumentElement({ elementType: "h5", innerText: `${stadium.venue}` }),
+                            DocumentElement({ elementType: "p", innerText: `${stadium.city}` })
+                        ]
+                    }),
+                    DocumentElement({ elementType: "p", className: "capacity", innerText: `${stadium.capacity}K` })
+                ],
+                listener: {
+                    event: "click",
+                    callBack: () => {
+                        if (this.stadiums.includes(stadium.venue)) {
+                            this.RemoveStadiums([stadium.venue]);
+                        }
+                        else {
+                            this.AddStadiums([stadium.venue]);
+                        }
+                    }
+                }
+            });
+            if (this.stadiums.includes(stadium.venue)) {
+                element.children[0].classList.remove("stadium-unselected");
+                element.children[0].classList.add("stadium-selected");
+            }
+            else {
+                element.children[0].classList.remove("stadium-selected");
+                element.children[0].classList.add("stadium-unselected");
+            }
+            stadiumContainer === null || stadiumContainer === void 0 ? void 0 : stadiumContainer.appendChild(element);
+        }
+        const gamesSection = document.querySelector(".games-section");
+        if (gamesSection === null) {
+            return;
+        }
+        const games = document.querySelectorAll(".game-collection");
+        games.forEach(x => gamesSection.removeChild(x));
+        const roundToggle = document.querySelector("#round-toggle");
+        const stadiumToggle = document.querySelector("#stadium-toggle");
+        if (roundToggle === null || stadiumToggle === null) {
+            return;
+        }
+        if (this.toggle === "round") {
+            roundToggle.classList.add("toggle-selected");
+            roundToggle.classList.remove("toggle-unselected");
+            stadiumToggle.classList.remove("toggle-selected");
+            stadiumToggle.classList.add("toggle-unselected");
+        }
+        else {
+            stadiumToggle.classList.add("toggle-selected");
+            stadiumToggle.classList.remove("toggle-unselected");
+            roundToggle.classList.remove("toggle-selected");
+            roundToggle.classList.add("toggle-unselected");
+        }
+        // console.log(GetGames(this.state, this.teams, this.stadiums, this.upcoming));
+        for (const [key, collection] of Object.entries(groupBy(GetGames(this.state, this.teams, this.stadiums, this.upcoming), game => this.toggle === "round" ? game.round : globalData.stadiums.find(s => s.id === game.stadiumId).venue))) {
+            const gameCollection = DocumentElement({
+                elementType: "div",
+                className: "game-collection"
+            });
+            gameCollection.appendChild(DocumentElement({ elementType: "h3", innerText: this.toggle === "round" ? `Round ${key}` : key }));
+            const games = DocumentElement({ elementType: "div", className: "games" });
+            for (const game of collection) {
+                const element = DocumentElement({
+                    elementType: "div",
+                    className: "game",
+                    children: [
+                        DocumentElement({
+                            elementType: "div",
+                            className: "top-bar",
+                            innerText: `${game.date} ${game.time}`
+                        }),
+                        DocumentElement({
+                            elementType: "div",
+                            className: "main-game",
+                            children: [
+                                DocumentElement({ elementType: "p", innerText: `${game.homeTeam.localName} ${game.homeTeam.teamName}` }),
+                                DocumentElement({ elementType: "p", innerText: "V" }),
+                                DocumentElement({ elementType: "p", innerText: `${game.awayTeam.localName} ${game.awayTeam.teamName}` })
+                            ]
+                        }),
+                        DocumentElement({
+                            elementType: "div",
+                            className: "image-overlays",
+                            children: [
+                                DocumentElement({ elementType: "img", id: "left", attribute: { name: "src", value: `image/facing/${game.homeTeam.teamName}Right.png` } }),
+                                DocumentElement({ elementType: "img", id: "right", attribute: { name: "src", value: `image/facing/${game.awayTeam.teamName}Left.png` } })
+                            ]
+                        }),
+                        DocumentElement({
+                            elementType: "div",
+                            className: "info-box",
+                            children: [
+                                DocumentElement({
+                                    elementType: "div",
+                                    className: "info-stadium",
+                                    children: [DocumentElement({ elementType: "p", id: "stadium-header", innerText: globalData.stadiums.find(s => s.id === game.stadiumId).venue })]
+                                }),
+                                DocumentElement({
+                                    elementType: "button",
+                                    id: "ticket-btn",
+                                    innerText: "Tickets ->",
+                                    // children: [DocumentElement({elementType: "a", className:"ticketAnchor"})],
+                                    // listener: { event: "click", callBack: () => { window.open(game.ticketUrl, "_blank").focus(); } }
+                                })
+                            ]
+                        })
+                    ]
+                });
+                games.appendChild(element);
+                gameCollection.appendChild(games);
+            }
+            gamesSection === null || gamesSection === void 0 ? void 0 : gamesSection.appendChild(gameCollection);
+        }
+    }
+}
+function GetStadiumsByTeamsAndState(state, teams) {
+    const stadiums = [];
+    for (const stadium of globalData.stadiums) {
+        if (stadium.state === state) {
+            for (const team of teams) {
+                if (TeamPlaysAt(team, stadium)) {
+                    if (!stadiums.includes(stadium)) {
+                        stadiums.push(stadium);
+                    }
+                }
+            }
+        }
+    }
+    return stadiums;
+}
+
+function TeamPlaysAt(team, stadium) {
+
+    for (const game of globalData.games) {
+        if (game.homeTeam.teamName === team || game.awayTeam.teamName === team) {
+            const gameStadium = globalData.stadiums.find(s => s.id === game.stadiumId);
+            if (gameStadium === stadium) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function GameInPast(game) {
+    const [day, monthName] = game.date.split(" ");
+    const monthIndex = new Date(`${monthName} 1`).getMonth();
+    const utcDate = new Date(Date.UTC(2026, monthIndex, day));
+    const ausDate = utcDate.toLocaleString("en-US", { timeZone: "Australia/Sydney" })
+    const now = new Date(
+        new Date().toLocaleString("en-US", { timeZone: "Australia/Sydney" })
+    );
+    return ausDate < now;
+}
+
+function GetGames(state, teams, stadiums, upcoming) {
+    console.log("hello???");
+    const games = [];
+    for (const stadium of stadiums) {
+        for (const game of globalData.games) {
+            // console.log(game.stadiumId);
+            // console.log(stadium.id);
+            // console.log(stadium);
+            console.log(game.stadiumId);
+            const sdfsdfsdf = globalData.stadiums.find(s => s.id === game.stadiumId);
+            console.log(sdfsdfsdf)
+            if (globalData.stadiums.find(s => s.id === game.stadiumId).venue === stadium) {
+                console.log(game);
+                if (upcoming && GameInPast(game)) {
+                    continue;
+                }
+                if (teams.includes(game.homeTeam.teamName) || teams.includes(game.awayTeam.teamName)) {
+                    games.push(game);
+                }
+            }
+        }
+    }
+    return games;
+}
+//TODO Refactor this
+function groupBy(array, keyFn) {
+    return array.reduce((result, item) => {
+        const key = keyFn(item);
+        if (!result[key]) {
+            result[key] = [];
+        }
+        result[key].push(item);
+        return result;
+    }, {});
+}
---
+++ src/main.js
@@ -0,0 +1,49 @@
+import { Filters } from "./classes/Filters.js";
+const filter = new Filters();
+//Change state filter on click
+const state_dropdown = document.querySelector("#state");
+if (state_dropdown) {
+    state_dropdown.onchange = () => {
+        filter.ChangeState(state_dropdown.value);
+    };
+}
+//Change filter to show only upcoming games when selected
+const upcomingCheckbox = document.querySelector("#upcomingCb");
+if (upcomingCheckbox) {
+    upcomingCheckbox.onclick = () => {
+        filter.upcoming = upcomingCheckbox.checked;
+        filter.OnChange();
+    };
+}
+//Change sort mode on the click of either button
+const roundToggle = document.querySelector("#round-toggle");
+const stadiumToggle = document.querySelector("#stadium-toggle");
+console.log(roundToggle, stadiumToggle);
+if (roundToggle && stadiumToggle) {
+    roundToggle.onclick = () => {
+        console.log("brah");
+        filter.toggle = "round";
+        filter.OnChange();
+    };
+    stadiumToggle.onclick = () => {
+        filter.toggle = "venue";
+        filter.OnChange();
+    };
+}
+//Allow scrolling through the stadium and team containers if content overflows
+const teamContainer = document.querySelector('.team-container');
+const stadiumContainer = document.querySelector(".stadium-options-container");
+if (teamContainer && stadiumContainer) {
+    stadiumContainer.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        stadiumContainer.scrollLeft += e.deltaY / 2;
+    });
+    teamContainer.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        teamContainer.scrollLeft += e.deltaY / 2;
+    });
+}
+//Rerun the filter when the page loads
+window.addEventListener('load', () => {
+    filter.OnChange();
+});
